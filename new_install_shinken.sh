a87e7ceaf8a3d3bca533c1f45d41ee0b1b727efe
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2016-12-06 22:16:40 +0100

    Update new_install_shinken.sh

--- new_install_shinken.sh
+++ new_install_shinken.sh
@@ -1,4 +1,4 @@
-apt-get install curl && curl -L http://install.shinken-monitoring.org | /bin/bash
+apt-get install -y curl && curl -L http://install.shinken-monitoring.org | /bin/bash
 service shinken stop
 chown -R shinken:shinken /usr/local/shinken
 service shinken start
