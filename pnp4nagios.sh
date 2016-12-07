ce82fc9056545640ea49be76b81aede3b16361a1
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2016-12-07 23:41:43 +0100

    Update pnp4nagios.sh

--- pnp4nagios.sh
+++ pnp4nagios.sh
@@ -1,3 +1,4 @@
+#http://www.samuelpoggioli.fr/shinken-2-installation-de-pnp4nagios/
 apt-get install -y apache2 php5 libapache2-mod-php5 rrdtool librrds-perl php5-gd
 wget --no-check-certificate http://downloads.sourceforge.net/project/pnp4nagios/PNP-0.6/pnp4nagios-0.6.24.tar.gz
 tar -xzf pnp4nagios-0.6.24.tar.gz
